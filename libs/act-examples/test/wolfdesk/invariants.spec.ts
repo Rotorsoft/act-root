28994e07db7cd3fc9f73a488bc3b499975e75e64
rotorsoft <rotorsoft@outlook.com>
2025-06-27 13:25:42 -0400

    chore: fix test

--- libs/act-examples/test/wolfdesk/invariants.spec.ts
+++ libs/act-examples/test/wolfdesk/invariants.spec.ts
@@ -1,4 +1,4 @@
-import { dispose, InvariantError } from "@rotorsoft/act";
+import { dispose } from "@rotorsoft/act";
 import { Chance } from "chance";
 import { afterAll, describe, expect, it } from "vitest";
 import {
@@ -36,87 +36,115 @@ describe("ticket invariants", () => {
 
   it("should throw when trying to close twice or empty", async () => {
     const t = target();
-    await expect(closeTicket(t)).rejects.toThrow(InvariantError);
+    await expect(closeTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(closeTicket(t)).rejects.toThrow(InvariantError);
+    await expect(closeTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when assigning agent to empty or closed ticket", async () => {
     const t = target();
-    await expect(assignTicket(t)).rejects.toThrow(InvariantError);
+    await expect(assignTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(assignTicket(t)).rejects.toThrow(InvariantError);
+    await expect(assignTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when adding message to empty or closed ticket", async () => {
     const t = target();
-    await expect(addMessage(t, "message")).rejects.toThrow(InvariantError);
+    await expect(addMessage(t, "message")).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(addMessage(t, "message")).rejects.toThrow(InvariantError);
+    await expect(addMessage(t, "message")).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when requesting escalation to empty or closed ticket", async () => {
     const t = target();
-    await expect(requestTicketEscalation(t)).rejects.toThrow(InvariantError);
+    await expect(requestTicketEscalation(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(requestTicketEscalation(t)).rejects.toThrow(InvariantError);
+    await expect(requestTicketEscalation(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when escalating empty or closed ticket", async () => {
     const t = target();
-    await expect(escalateTicket(t)).rejects.toThrow(InvariantError);
+    await expect(escalateTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(escalateTicket(t)).rejects.toThrow(InvariantError);
+    await expect(escalateTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when reassigning empty or closed ticket", async () => {
     const t = target();
-    await expect(reassignTicket(t)).rejects.toThrow(InvariantError);
+    await expect(reassignTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(reassignTicket(t)).rejects.toThrow(InvariantError);
+    await expect(reassignTicket(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when marking messages delivered on empty or closed or invalid ticket", async () => {
     const t = target();
-    await expect(markMessageDelivered(t, chance.guid())).rejects.toThrow(
-      InvariantError
-    );
+    await expect(markMessageDelivered(t, chance.guid())).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await expect(markMessageDelivered(t, chance.guid())).rejects.toThrow(
       MessageNotFoundError
     );
     await closeTicket(t);
-    await expect(markMessageDelivered(t, chance.guid())).rejects.toThrow(
-      InvariantError
-    );
+    await expect(markMessageDelivered(t, chance.guid())).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when marking message read on empty or closed or invalid ticket", async () => {
     const t = target();
-    await expect(acknowledgeMessage(t, chance.guid())).rejects.toThrow(
-      InvariantError
-    );
+    await expect(acknowledgeMessage(t, chance.guid())).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await expect(acknowledgeMessage(t, chance.guid())).rejects.toThrow(
       MessageNotFoundError
     );
     await closeTicket(t);
-    await expect(acknowledgeMessage(t, chance.guid())).rejects.toThrow(
-      InvariantError
-    );
+    await expect(acknowledgeMessage(t, chance.guid())).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 
   it("should throw when resolving empty or closed ticket", async () => {
     const t = target();
-    await expect(markTicketResolved(t)).rejects.toThrow(InvariantError);
+    await expect(markTicketResolved(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
     await openTicket(t, "opening once", "the first opening");
     await closeTicket(t);
-    await expect(markTicketResolved(t)).rejects.toThrow(InvariantError);
+    await expect(markTicketResolved(t)).rejects.toMatchObject({
+      name: "ERR_INVARIANT",
+    });
   });
 });
