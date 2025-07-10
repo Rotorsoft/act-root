5d41425ce490e5bb3534ed272b4cba4d06fa3c4d
rotorsoft <rotorsoft@outlook.com>
2025-07-10 09:02:52 -0400

    fix: up and bump

--- libs/act-pg/src/index.ts
+++ libs/act-pg/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @packageDocumentation
  * @module act-pg
- * Main entry point for the Act-PG framework. Re-exports all core APIs.
+ * Main entry point for the Act-PG framework. Re-exports all core APIs
  */
 export * from "./PostgresStore.js";
